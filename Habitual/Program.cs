70c3eeb66bf4c2c339aa05e08718daa62615e904
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-10-30 21:41:06 -0400

    Changed the startup url.

--- Habitual/Program.cs
+++ Habitual/Program.cs
@@ -19,6 +19,6 @@ namespace Habitual
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>().UseUrls("http://localhost:5000");
     }
 }
