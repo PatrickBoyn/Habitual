ad1b2bfb7461e9e39b7f536956e49d04cdf64b9c
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-11-02 02:19:06 -0400

    Added Cors.

--- Habitual/Startup.cs
+++ Habitual/Startup.cs
@@ -27,6 +27,7 @@ namespace Habitual
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddDbContext<HabitContext>(opt => opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
@@ -44,6 +45,7 @@ namespace Habitual
             }
 
             app.UseHttpsRedirection();
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseMvc();
         }
     }
