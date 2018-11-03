65f721f3a78b6fb9b07b4977c329cf899030afbb
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-11-02 22:26:57 -0400

    Added a register, login and boolean method to check if the user exists or not.

--- Habitual/Data/IAuthRepository.cs
+++ Habitual/Data/IAuthRepository.cs
@@ -1,7 +1,12 @@
+using System.Threading.Tasks;
+using Habitual.Models;
+
 namespace Habitual.Data
 {
     public interface IAuthRepository
     {
-        
+        Task<User> Register(User user, string password);
+        Task<User> Login(string username, string password);
+        Task<bool> UserExists(string username);
     }
 }
