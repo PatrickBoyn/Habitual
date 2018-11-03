8db4a86bd669a90733c858087df3a2ca238f02fe
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-11-03 17:15:21 -0400

    Added the controller and route attributes. Also added the ctor and initialized the feild. Also created the basic form of the register method.

--- Habitual/Controllers/AuthController.cs
+++ Habitual/Controllers/AuthController.cs
@@ -1,7 +1,25 @@
+using System.Threading.Tasks;
+using Habitual.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+
 namespace Habitual.Controllers
 {
-    public class AuthController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
     {
-        
+        private readonly IAuthRepository _repo;
+
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(string username, string password)
+        {
+            //TODO validate request
+        }
     }
 }
