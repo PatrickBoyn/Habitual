d9f1649fd4a72638e1e353197400719cda817d77
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-11-11 03:27:11 -0500

    Created a model object to store the user model in. Also created the login method. This is to test that it works.

--- Habitual-SPA/src/app/login/login.component.ts
+++ Habitual-SPA/src/app/login/login.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  model: object = {};
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  login(){
+    console.log(this.model);
   }
 
 }
