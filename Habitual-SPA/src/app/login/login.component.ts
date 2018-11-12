936c4ed3250249c2ab27a8e063760b8614620e16
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-11-11 20:00:24 -0500

    Minor bug fixing.  It wouldn't work as a strongly typed object.

--- Habitual-SPA/src/app/login/login.component.ts
+++ Habitual-SPA/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../_services/auth.service";
 
 @Component({
   selector: 'app-login',
@@ -6,15 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  model: object = {};
+  model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   login(){
-    console.log(this.model);
+    this.authService.login(this.model).subscribe(next => {
+      console.log('Login success!');
+    }, error => {
+      console.log('Something went wrong.')
+    });
   }
 
+  loggedIn(){
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout(){
+    localStorage.removeItem('token');
+    console.log('Logged out.');
+  }
 }
